fae70b99e279fb938bac067f7f5815e031ead1b9
Allison <azywong@gmail.com>
2014-04-30 20:22:54 -0700

    added challenge 1 & 3 solution

--- week_8_and_9/1_git_started/my_solution.md
+++ week_8_and_9/1_git_started/my_solution.md
@@ -3,46 +3,86 @@ Define the following commands and describe how they work/what they do.
 
 
 #### add
-<!-- Your defnition here -->
+adds file to staging area so git can keep track of it
 
 #### branch
-<!-- Your defnition here -->
+creates a new branch (an alternate set of files based on the master set where changes to the branch take place on their own)
 
 #### checkout
-<!-- Your defnition here -->
+goes to that file and gets rid of all changes since that commit.  Or goes to that branch.
 
 #### clone
-<!-- Your defnition here -->
+clones the repository in the new directory
 
 #### commit
-<!-- Your defnition here -->
+commits changes.  Like taking a snapshot of changes since the last commit
 
 #### fetch
-<!-- Your defnition here -->
+retrieves any commits that you don't have and stores them locally
 
 #### log
-<!-- Your defnition here -->
+pulls up all changes committed so far
 
 #### merge
-<!-- Your defnition here -->
+combines commits of the specified branches
 
 #### pull
-<!-- Your defnition here -->
+takes commits from the specified source and combines with the current branch.
 
 #### push
-<!-- Your defnition here -->
+pushes commits to the remote repository hosted on github
 
 #### reset
-<!-- Your defnition here -->
+undoes the last commit
 
 #### rm
-<!-- Your defnition here -->
+removes the file(s)
 
 #### status
+shows status of repository
 
 
 ## Release 3: Git Workflow
 
 - Push files to a remote repository
+```
+git push origin master
+```
+
 - Fetch changes
+```
+git fetch upstream
+git merge upstream/master
+```
+
 - Commit locally
+```
+git commit -m "comment"
+```
+
+
+## Reflect
+
+#### What parts of your strategy worked? What problems did you face?
+I went through and read a bunch of the resources on git and jotted down some notes.  I didn't understand the first resource, so I kept on
+going and reading more that explained things in different ways.
+
+#### What questions did you have while coding? What resources did you find to help you answer them?
+The questions I had were mainly answered by the linked resources.  Any questions I still have can be traced back to my general shakey-ness
+with using git.
+
+#### What concepts are you having trouble with, or did you just figure something out? If so, what?
+I'm having trouble with some of the general commands in git, just how they work and when I'd need to use them.  I'm still not quite sure I
+totally understand reset and what's going on.
+
+#### Did you learn any new skills or tricks?
+This is all entirely new to me so I guess new skill is using git from the terminal.
+
+#### How confident are you with each of the Learning Competencies?
+I'm not super confident with the learning competencies.  I think I can use git in the terminal at a basic level, but thats it.
+
+#### Which parts of the challenge did you enjoy?
+Not much really.
+
+#### Which parts of the challenge did you find tedious?
+It was frustrating to go through so many wordy resources and still not really understand what was going on.
--- week_8_and_9/3_JavaScript/gradebook/my_solution.js
+++ week_8_and_9/3_JavaScript/gradebook/my_solution.js
@@ -1,3 +1,23 @@
+
+/*
+Psuedocode
+1. create variable average = function syntax
+2. input = array of numbers
+    
+    output = average
+3. create variable gradebook = new object
+4. make all students properties of gradebook
+    make them all objects containing testScores
+5. make a function addScore
+    make that function property of gradebook
+    takes arguements name and score
+    loop through students
+    if match is found, push score into corresponding scores array
+6. create function getAverage
+    make property of gradebook
+    takes arguement name
+    assign it property of a function that returns student's average
+
 /*
 U3.W8-9: Gradebook from Names and Scores
 
@@ -7,7 +27,7 @@ variables match (i.e., 'Joseph' is the first element in students; his scores are
 
 Do not alter the students and scores code.
 
-I worked on this challenge [by myself, with:]
+I worked on this challenge [with: Andra Lally]
 
 */
 
@@ -18,33 +38,131 @@ var scores = [ [80, 70, 70, 100],
                [75, 70, 80, 75],
                [100, 90, 95, 85] ]
 
-
-
-
-
-
 // __________________________________________
 // Write your code below.
 
+/*
+var average = function(array) {
+    sum = 0
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i]; 
+    };
+    avg = sum/array.length;
+    return avg;
+};
 
+var gradebook = {
 
+    Joseph: {
+        testScores: scores[0]
+    },
+    Susan: {
+        testScores: scores[1]
+    },
+    William: {
+        testScores: scores[2]
+    },
+    Elizabeth: {
+        testScores: scores[3]
+    },
 
-
+    addScore: function(name, score) {
+        for (var i = 0; i < students.length ; i++) {
+            if (name === students[i])
+            {
+              scores[i].push(score);
+            }
+        }
+    },
+    getAverage: function(name) {
+        for (var i = 0; i < students.length ; i++) {
+            if (name === students[i])
+            {
+                return average(scores[i]);
+            }
+        }      
+    }
+};
+*/
 
 // __________________________________________
 // Refactored Solution
 
+var average = function(array) {
+    sum = 0
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i]; 
+    };
+   return sum/array.length;
+};
 
+var gradebook = {
 
+    Joseph: {
+        testScores: scores[0]
+    },
+    Susan: {
+        testScores: scores[1]
+    },
+    William: {
+        testScores: scores[2]
+    },
+    Elizabeth: {
+        testScores: scores[3]
+    },
 
-
-
+    addScore: function(name, score) {
+        for (var i = 0; i < students.length ; i++) {
+            if (name === students[i])
+            {
+              scores[i].push(score);
+            }
+        }
+    },
+    getAverage: function(name) {
+        for (var i = 0; i < students.length ; i++) {
+            if (name === students[i])
+            {
+                return average(scores[i]);
+            }
+        }      
+    }
+};
 
 
 // __________________________________________
 // Reflect
 
+/*
+What parts of your strategy worked? What problems did you face?
 
+We started out by pseudocoding, but ran into issues understanding what the instructions were asking for exactly since neither of us
+was very strong with javascript.  Looking up syntax in from our notes/resources helped a lot, but progress was slow and involved lots
+of searching.
+
+What questions did you have while coding? What resources did you find to help you answer them?
+I mainly referenced notes and the linked resource for syntax questions.  Most of my questions were just syntax related because I'm
+not super familiar with javascript.
+
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+I think I'm get the logic behind the code, but getting the correct format for javascript throws me off sometimes.  Earlier, our code
+wasn't working because of a missing comma.  I think I need to be more careful with javascript because I'm not as used to it and its
+easier to make mistakes.
+
+Did you learn any new skills or tricks?
+Not really, more or less went over implementing what we've learned before, maybe not necessarily in javascript though.  Got more practice
+with javascript objects, which was good.
+
+How confident are you with each of the Learning Competencies?
+Not very confident.  I had to use a lot of references to get this challenge done and it took a while.
+
+Which parts of the challenge did you enjoy?
+When the code finally worked!
+
+Which parts of the challenge did you find tedious?
+Going back and forth between the assignment and googling because we didn't understand the wording of the assignment.
+
+*/
 
 
 
@@ -54,7 +172,6 @@ var scores = [ [80, 70, 70, 100],
 
 // __________________________________________
 // Driver Code:  Do not alter code below this line.
-
 
 function assert(test, message, test_number) {
   if (!test) {
