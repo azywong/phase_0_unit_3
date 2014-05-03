3c960c98c18c47acc8a5a673a9ba2e37dd5d4c5a
Allison <azywong@gmail.com>
2014-05-02 23:23:47 -0700

    ruby challenge solution

--- week_8_and_9/4_Ruby/fibonacci_sequence/my_solution.rb
+++ week_8_and_9/4_Ruby/fibonacci_sequence/my_solution.rb
@@ -1,16 +1,39 @@
 # U3.W8-9: 
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
+=begin
+note: for fibonacci number: to test if  5x^2 + 4 or 5x^2 -4 is perfect square
 
-
+function takes number as input
+	squared = num*num
+	condition_a = 5*squared + 4
+	condition_b = 5*squared - 4
+	
+	if square root of condition a is a perfect square
+		return true
+	elsif square root of condition b is a perfect square
+		return true
+	else
+		return false
+=end
 
 # 3. Initial Solution
 
 def is_fibonacci?(num)
+	squared = num * num
+	condition_a = 5 * squared + 4
+	condition_b = 5 * squared - 4
 
+	if Math.sqrt(condition_a) == Math.sqrt(condition_a).to_i
+		return true
+	elsif Math.sqrt(condition_b) == Math.sqrt(condition_b).to_i
+		return true
+	else
+		return false	
+	end
 end
 
 
@@ -24,8 +47,12 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
-
+def assert
+	raise "Assertion failed!" unless yield
+end
+assert { is_fibonacci?(0) == true }
+assert { is_fibonacci?(1) == true }
+assert { is_fibonacci?(10946) == true }
 
 
 
--- week_8_and_9/4_Ruby/fizz_buzz/my_solution.rb
+++ week_8_and_9/4_Ruby/fizz_buzz/my_solution.rb
@@ -1,24 +1,61 @@
 # U3.W8-9: 
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
+=begin
+iterate through the array
+make empty array
+	if i%15 == 0
+		shovel FizzBuzz
+	elsif i%5 == 0
+		shovel Buzz
+	elsif i%3 == 0	
+		shovel Fizz
+	else
+		shovel i 
+=end
 
 
 # 3. Initial Solution
 
-def super_fizzbuzz(array)
-
-
-end
+# def super_fizzbuzz(array)
+# supered = []
+# 	array.each do |x|
+# 		if x % 15 == 0
+# 			supered.push("FizzBuzz")
+# 		elsif x % 5 == 0
+# 			supered.push("Buzz")
+# 		elsif x % 3 == 0
+# 			supered.push("Fizz")
+# 		else
+# 			supered.push(x)
+# 		end		
+# 	end	
+# 	return supered
+# end
 
 
 
 # 4. Refactored Solution
 
-
+def super_fizzbuzz(array)
+supered = []
+	array.each do |x|
+		if x % 15 == 0
+			supered << "FizzBuzz"
+		elsif x % 5 == 0
+			supered << "Buzz"
+		elsif x % 3 == 0
+			supered << "Fizz"
+		else
+			supered << x
+		end		
+	end	
+	return supered
+end
 
 
 
@@ -26,8 +63,45 @@ end
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
 
-
+def assert
+	raise "Assertion failed!" unless yield
+end
+assert { super_fizzbuzz([1,2,3])  == [1, 2, "Fizz"]}
+assert { super_fizzbuzz([1,2,5])  == [1, 2, "Buzz"]}
+assert { super_fizzbuzz([1,2,15]) == [1, 2, "FizzBuzz"]}
+assert { super_fizzbuzz([30, 9, 20, 1]) == ["FizzBuzz", "Fizz", "Buzz", 1]}
 
 
 
 # 5. Reflection 
+=begin
+What parts of your strategy worked? What problems did you face?
+I basically thought the problem out and pseudocoded and then coded the solution.  At first I was trying to modify the original
+array and that didn't work quite as I hoped, so I moved to creating a new super-ed array and that worked a lot more smoothly.
+I didn't really face major problems outside that though.
+
+
+What questions did you have while coding? What resources did you find to help you answer them?
+I didn't really have any major questions while coding, but I referenced ruby docs a few times fora refresher on arrays.
+
+
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+The logic of the problem (order of the if statements) wasn't bad, but the implementation tripped me up a bit.  I didn't have
+too much trouble though.
+
+
+Did you learn any new skills or tricks?
+Not really, considering this week is review.
+
+
+How confident are you with each of the Learning Competencies?
+I think I'm alright with the learning competencies this time!
+
+
+Which parts of the challenge did you enjoy?
+I enjoyed thinking through the problem and getting the code to work
+
+
+Which parts of the challenge did you find tedious?
+None
+=end
